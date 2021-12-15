e3c94a90e47d55da113980db7e97a3d57bb41a6d
burakyccl <55767999+burakyccl@users.noreply.github.com>
2021-12-15 21:16:21 +0300

    Home_v1
    İçime sinen ilk versiyon

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex justify-content-around">
-    <div class="card bg-info">
+    <div class="card bg-info shadow">
       <img src="../assets/zeytin.png" class="card-img" alt="zeytin" />
       <div class="card-img-overlay">
         <a href="#" class="text-decoration-none">
@@ -9,7 +9,7 @@
       </div>
     </div>
 
-    <div class="card bg-info">
+    <div class="card bg-info shadow">
       <img src="../assets/sulu.png" class="card-img" alt="sulu" />
       <div class="card-img-overlay">
         <a href="#" class="text-decoration-none">
@@ -18,7 +18,7 @@
       </div>
     </div>
 
-    <div class="card bg-info">
+    <div class="card bg-info shadow">
       <img src="../assets/hamur.png" class="card-img" alt="Hamur" />
       <div class="card-img-overlay">
         <a href="#" class="text-decoration-none">
@@ -27,7 +27,7 @@
       </div>
     </div>
 
-    <div class="card bg-info">
+    <div class="card bg-info shadow">
       <img src="../assets/vegan.png" class="card-img" alt="vegan" />
       <div class="card-img-overlay">
         <a href="#" class="text-decoration-none">
@@ -36,7 +36,7 @@
       </div>
     </div>
 
-    <div class="card bg-info">
+    <div class="card bg-info shadow">
       <img src="../assets/tatli.png" class="card-img" alt="tatli" />
       <div class="card-img-overlay">
         <a href="#" class="text-decoration-none">
@@ -48,8 +48,8 @@
 
   <hr class="featurette-divider" />
 
-  <div class="row featurette">
-    <div class="col-md-5 mt-3">
+  <div class="row featurette justify-content-center">
+    <div class="col-md-4 mt-3">
       <h2 class="featurette-heading display-3">Günün Menüsü</h2>
       <h2 class="featurette-heading display-5 text-muted">Kurufasulye Pilav</h2>
     </div>
@@ -79,6 +79,213 @@
   </div>
 
   <hr class="featurette-divider" />
+
+  <div class="container justify-content-center">
+    <div class="row row-cols-1 row-cols-md-4 g-3">
+      <div class="col">
+        <div class="card shadow-sm">
+          <svg
+            class="bd-placeholder-img card-img-top"
+            width="100%"
+            height="225"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: Thumbnail"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#55595c" />
+            <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+          </svg>
+
+          <div class="card-body">
+            <p class="card-text fs-3 fw-bolder">ÇiBörek Tarifi</p>
+            <div class="d-flex justify-content-center align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-success">
+                  Tarifi Görüntüle
+                </button>
+                <button type="button" class="btn btn-sm btn-outline-danger">
+                  <i class="bi bi-heart"></i>
+                </button>
+              </div>
+            </div>
+            <div class="container">
+             <a
+                href="#"
+                class="d-flex link-dark text-decoration-none pt-2"
+                aria-expanded="false"
+              >
+                <img
+                  src="https://github.com/mdo.png"
+                  alt="mdo"
+                  width="50"
+                  height="50"
+                  class="rounded-circle"
+                />
+                <p class="fs-4 ps-3">Burak Yucel</p>
+              </a>
+              <small class="text-muted">10 dakika 5 kisilik</small>
+            </div>
+          </div>
+        </div>
+      </div>
+
+       <div class="col">
+        <div class="card shadow-sm">
+          <svg
+            class="bd-placeholder-img card-img-top"
+            width="100%"
+            height="225"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: Thumbnail"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#55595c" />
+            <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+          </svg>
+
+          <div class="card-body">
+            <p class="card-text fs-3 fw-bolder">ÇiBörek Tarifi</p>
+            <div class="d-flex justify-content-center align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-success">
+                  Tarifi Görüntüle
+                </button>
+                <button type="button" class="btn btn-sm btn-outline-danger">
+                  <i class="bi bi-heart"></i>
+                </button>
+              </div>
+            </div>
+            <div class="container">
+             <a
+                href="#"
+                class="d-flex link-dark text-decoration-none pt-2"
+                aria-expanded="false"
+              >
+                <img
+                  src="https://github.com/mdo.png"
+                  alt="mdo"
+                  width="50"
+                  height="50"
+                  class="rounded-circle"
+                />
+                <p class="fs-4 ps-3">Burak Yucel</p>
+              </a>
+              <small class="text-muted">10 dakika 5 kisilik</small>
+            </div>
+          </div>
+        </div>
+      </div>
+
+       <div class="col">
+        <div class="card shadow-sm">
+          <svg
+            class="bd-placeholder-img card-img-top"
+            width="100%"
+            height="225"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: Thumbnail"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#55595c" />
+            <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+          </svg>
+
+          <div class="card-body">
+            <p class="card-text fs-3 fw-bolder">ÇiBörek Tarifi</p>
+            <div class="d-flex justify-content-center align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-success">
+                  Tarifi Görüntüle
+                </button>
+                <button type="button" class="btn btn-sm btn-outline-danger">
+                  <i class="bi bi-heart"></i>
+                </button>
+              </div>
+            </div>
+            <div class="container">
+             <a
+                href="#"
+                class="d-flex link-dark text-decoration-none pt-2"
+                aria-expanded="false"
+              >
+                <img
+                  src="https://github.com/mdo.png"
+                  alt="mdo"
+                  width="50"
+                  height="50"
+                  class="rounded-circle"
+                />
+                <p class="fs-4 ps-3">Burak Yucel</p>
+              </a>
+              <small class="text-muted">10 dakika 5 kisilik</small>
+            </div>
+          </div>
+        </div>
+      </div>
+
+       <div class="col">
+        <div class="card shadow-sm">
+          <svg
+            class="bd-placeholder-img card-img-top"
+            width="100%"
+            height="225"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: Thumbnail"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#55595c" />
+            <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+          </svg>
+
+          <div class="card-body">
+            <p class="card-text fs-3 fw-bolder">ÇiBörek Tarifi</p>
+            <div class="d-flex justify-content-center align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-success">
+                  Tarifi Görüntüle
+                </button>
+                <button type="button" class="btn btn-sm btn-outline-danger">
+                  <i class="bi bi-heart"></i>
+                </button>
+              </div>
+            </div>
+            <div class="container">
+             <a
+                href="#"
+                class="d-flex link-dark text-decoration-none pt-2"
+                aria-expanded="false"
+              >
+                <img
+                  src="https://github.com/mdo.png"
+                  alt="mdo"
+                  width="50"
+                  height="50"
+                  class="rounded-circle"
+                />
+                <p class="fs-4 ps-3">Burak Yucel</p>
+              </a>
+              <small class="text-muted">10 dakika 5 kisilik</small>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      
+
+    </div>
+  </div>
 </template>
 
 <script>
